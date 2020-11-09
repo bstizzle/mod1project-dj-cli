a3594a2ba86850f85ca9cbe9b0d5fe6b16e1674b
bstizzle <whereyouben@gmail.com>
2020-11-09 12:52:15 -0500

    Set up environment and tested Spotify API and RSpotify rubygem

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "https://rubygems.org"
+
+gem 'activerecord'
+gem 'sinatra-activerecord'
+gem 'rake'
+gem 'require_all'
+gem 'sqlite3'
+gem 'pry'
+# make sure you're updating this file and adding gems you use in your project
+
+#added gems
+gem 'rspotify'
---
+++ config/environment.rb
@@ -0,0 +1,15 @@
+require 'bundler/setup'
+Bundler.require
+
+RSpotify::authenticate("a1c02f8e677c46b5b12b8061b4017657", "c15f867c496d49d586e46af820375cca")
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: "db/development.sqlite3"
+)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+# Comment out line above and uncomment out line below to remove logger when presenting
+# ActiveRecord::Base.logger =  nil
+
+require_all 'lib'
